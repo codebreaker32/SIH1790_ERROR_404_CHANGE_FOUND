662dbacbd649d05f95a56e506c3dd98523afcf46
Anshika Singh <20anshika.singh10b@gmail.com>
2024-09-30 02:51:03 +0530

    Copied files from recover to frontend directory

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import AdminLogin from './components/adminlogin';
+import Signup from './components/signup';
+import Roles from './components/roles';
+import BranchLogin from './components/branchlogin'; 
+import StaffDashboard from './components/staffdash';
+import UserDashboard from './components/userdash';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Roles />} /> 
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/branch-login" element={<BranchLogin />} /> 
+	  <Route path="/staff-dashboard" element={<StaffDashboard />} />
+	  <Route path="/user-dashboard" element={<UserDashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
